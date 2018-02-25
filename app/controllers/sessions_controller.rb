3bb5ac22b627953a62fd6b9ea90be45d162cab62
5-4 <github email address>
2018-02-25 17:24:39 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,19 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		@user = User.find_by(name: params[:name])
-		redirect_to login_path unless @user.authenticate(params[:password])
-		session[:user_id] = @user.id
-		redirect_to welcome_path
+		@user = User.find_by(name: user_params[:name])
+		if @user && @user.authenticate(user_params[:password])
+			session[:user_id] = @user.id
+			redirect_to welcome_path
+		else
+			redirect_to login_path
+		end
 	end
+
+		private
+	  
+	  def user_params
+	    params.require(:user).permit(:name, :password, :id)
+	  end
 
 end
